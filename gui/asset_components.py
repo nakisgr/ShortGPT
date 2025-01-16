2195fef9fe9efb6c79d5e1fa2d640aa436e6d476
Georgios Nakis <georgios.nakis.gr@gmail.com>
2025-01-16 23:21:39 +0100

    fIX DEFAULT

--- gui/asset_components.py
+++ gui/asset_components.py
@@ -67,7 +67,7 @@ class AssetComponentsUtils:
                 choices=choices,
                 interactive=True,
                 label="Choose background video",
-                value="Music motivation"
+                value=random.choice(choices)
             )
         return cls.instance_background_video_checkbox
 
@@ -79,7 +79,7 @@ class AssetComponentsUtils:
                 choices=choices,
                 interactive=True,
                 label="Choose background music",
-                value="Walking on the beach"
+                value=random.choice(choices)
             )
         return cls.instance_background_music_checkbox
 
